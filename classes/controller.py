704031e4edff904a2c6cb58602ee756633aca87d
imconnorngl <developer@purpled.host>
2021-02-12 10:00:27 +0000

    ingame position class
    tracks the position of a user through the use of the arrow keys. Intergrates directly through the "controller" class

--- classes/controller.py
+++ classes/controller.py
@@ -1,6 +1,24 @@
 class Controller:
     def __init__(self):
         self.position = { "x": 0, "y": 0 }
-    
     def onPress(self, key):
-        print(key)
+        try:
+            k = key.char
+        except:
+            k = key.name
+
+        if k in ['left', 'right', 'up', 'down']:
+            directions = {
+                "left": { "x": 1, "y": 0 },
+                "right": { "x": -1, "y": 0 },
+                "up": { "x": 0, "y": 1 },
+                "down": { "x": 0, "y": -1 },
+            }
+
+            self.moveBy(directions[k])
+    
+    def moveBy(self, movement):
+        self.position["x"] += movement["x"]
+        self.position["y"] += movement["y"]
+
+        print(self.position)
